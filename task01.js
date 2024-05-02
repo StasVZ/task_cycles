415edaa7aedd2fb1215e98641ae28292f838c206
StasVZ <stimyl89@mail.ru>
2024-05-02 09:20:18 +0300

    task

---
+++ task01.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for (let m = 2 ; m < 10 ; m++) {
+  console.log ('_____________________________');
+  for (let a = 1; a <= 10 ;a++){
+    console.log(`${a} в степени ${m} = ${a**m}`);
+  }
+}
